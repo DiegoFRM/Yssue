9d8a2ee2cafafaaa60ebb4021fdaf267e9f527ef
DaveSanG <davidsanchezgarduo@gmail.com>
2018-11-12 23:54:07 -0600

    images test

--- Curso_evacuacion/AR_webapp/js/spineController.js
+++ Curso_evacuacion/AR_webapp/js/spineController.js
@@ -229,14 +229,14 @@ console.log("loadSpine")
 		capa3Mesh.material.map = layerTexture3[0];
 		capa2Mesh.material.map = layerTexture2[0];
 		capa1Mesh.material.map = layerTexture1[0];
-		answerResponse.material.map = correctTexture;
+		//answerResponse.material.map = correctTexture;
 		break;
 		case "ar1B":
 		capa3Mesh.visible = true;
 		capa3Mesh.material.map = layerTexture3[1];
 		capa2Mesh.material.map = layerTexture2[1];
 		capa1Mesh.material.map = layerTexture1[1];
-		answerResponse.material.map = dangerTexture;
+		//answerResponse.material.map = dangerTexture;
 		number = 4;
 		scale = 0.05;
 		z = 15;
