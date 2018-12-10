4c18f50bda123c52668a1e98caea611df0b0bd04
Diego Rojas Marcial <diego@yogome.com>
2018-12-10 15:46:52 -0600

    update new video

--- Curso_evacuacion/src/js/main.js
+++ Curso_evacuacion/src/js/main.js
@@ -88,7 +88,8 @@ var module1 = [
 var module2 = [
     {slide:"slide9",name:"Tema 2: Definición de evacuación"},
     {slide:"slide10",name:"Evacuación"},
-    {slide:"slide11",name:"Evacuación"}
+    {slide:"slide11",name:"Evacuación"},
+    {slide:"slide12",name:"Evacuación"}
 ]
 
 function loadModule(moduloChoice){
